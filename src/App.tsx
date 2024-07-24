136a31c7a0394724dabd7521a112dfe74f2b3e6a
huynhlan158 <huynhlan158@gmail.com>
2024-07-24 16:53:50 +0700

    config prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <h1 className="text-red-500 text-3xl font-bold underline">Hello world!</h1>
-  );
+  return <h1 className="text-red-500 text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
