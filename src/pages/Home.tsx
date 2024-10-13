08c0cef8cc3683ee26389fc2235cbb977f17e67d
huynhlan158 <huynhlan158@gmail.com>
2024-10-13 08:08:26 +0700

    test get method on vercel deployment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { getTab1Items } from '~/services/tab1/fetch.tab1';
+import { Tab1Item } from '~/services/tab1/models/tab1Item';
+import { LoadingState } from '~/components/Layouts';
 import { Heading, Text } from '~/components/TypoGraphy';
 
 export function HomePage() {
   const { t } = useTranslation();
 
+  const [items, setItems] = useState<Tab1Item[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const tab1Items = await getTab1Items();
+      setItems(tab1Items);
+      setIsLoading(false);
+    })();
+  }, []);
+
   return (
     <>
       <Heading text={t('welcome')} />
       <Text text={t('welcome')} />
+
+      {items.map((item) => (
+        <div key={item.id}>
+          <Heading level={5} text={item.name} size="sm" />
+          <Text text={item.description} />
+        </div>
+      ))}
+
+      {isLoading && <Text text={t('loading-state')} />}
+
+      {isLoading && <LoadingState />}
     </>
   );
 }
