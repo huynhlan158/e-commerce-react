6cdf9a70e2dbf9d4547334341dd93eef47ac2468
huynhlan158 <huynhlan158@gmail.com>
2024-10-14 11:39:23 +0700

    update the maximum-scale in viewport only for iPhone

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,18 @@ import '~/App.css';
 
 export default function App() {
   useEffect(() => {
+    // Stop the auto-zooming in Safari web on iPhone when focusing a text input,
+    // but still leave users with the full ability to pinch and zoom.
+    if (navigator.userAgent.indexOf('iPhone') > -1) {
+      document
+        .querySelector('[name=viewport]')
+        ?.setAttribute(
+          'content',
+          'width=device-width, initial-scale=1, maximum-scale=1'
+        );
+    }
+
+    // Setup the mock API server.
     const server = setupServer();
     return () => server.shutdown();
   }, []);
